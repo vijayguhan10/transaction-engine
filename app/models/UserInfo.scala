7fb5e77672ceb15a02032758b7602055455c00f3
vijayguhan10 <vijayguhan10@gmail.com>
2026-04-13 20:25:44 +0530

    Removed the source files

--- app/models/UserInfo.scala
+++
@@ -1,9 +0,0 @@
-package models
-
-import play.api.libs.json.{Format, Json}
-
-final case class UserInfo(emailid: String, usrid: String, password: String)
-
-object UserInfo {
-  implicit val format: Format[UserInfo] = Json.format[UserInfo]
-}
