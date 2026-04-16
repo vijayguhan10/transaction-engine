b96e8f74b6547357c7608e2480c9d39e6bf6031f
vijayguhan10 <vijayguhan10@gmail.com>
2026-04-16 19:04:30 +0530

    Updated with the Models and user scala lass with the object as a singelton

---
+++ app/models/User.scala
@@ -0,0 +1,12 @@
+package models
+import play.api.libs.json._
+
+final case class User(
+    id:option[Long],
+    name:String,
+    age:Int,
+    address:String
+)
+object User{
+    implicit val format:OFormat[User]=Json.format[User]
+}
