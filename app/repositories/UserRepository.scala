99bb8729060d408c94be831c4209884d9947235b
vijayguhan10 <vijayguhan10@gmail.com>
2026-04-22 11:23:00 +0530

    Used the Inject form the guice and how the proejction works and how the Rep.? internally mapping and how the constructor maps the singleton database  , IMplicit automapping and internal behaviour how the compiler fetches the implicit

--- app/repositories/UserRepository.scala
+++ app/repositories/UserRepository.scala
@@ -1,12 +1,19 @@
 package repositories
 import javax.inject._
-import scala.concurrent.{Future, ExecutionContext}
 import slick.jdbc.PostgresProfile.api._
 import models.User
-@singleton
+import scala.concurrent.{ExecutionContext, Future}
+@Singleton
 class UserRepository @Inject() (db: Database)(implicit ec: ExecutionContext) {
   class UserTable(tag: Tag) extends Table[User](tag, "users") {
-    def id = column[Long]("id", o.PrimaryKey, o.AutoInc)
-
+    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+    def name = column[String]("name")
+    def age = column[Int]("age")
+    def address = column[String]("address")
+    def*=(id.?,name , age,address)<>(User.tupled,User,unapply)
+  }
+  private val users=TableQuery[UserTable]
+  def create (user:User):Future[User]={
+    val insertQuery=
   }
 }
